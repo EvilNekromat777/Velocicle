28bff4011f4d976b8f94d1a958a8ebaf65fecdd9
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-12-15 16:36:38 +0300

    feat: brands first row is finished

--- scripts/script.js
+++ scripts/script.js
@@ -27,10 +27,10 @@ ready(() => {
 	});
 
 	// //IBG
-	// document.querySelectorAll(".ibg").forEach(el => {
-	// 	if(el.querySelector('img')){
-	// 		el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
-	// 		el.querySelector('img').style.display = 'none';
-	// 	}
-	// });
+	document.querySelectorAll(".ibg").forEach(el => {
+		if(el.querySelector('img')){
+			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+			el.querySelector('img').style.display = 'none';
+		}
+	});
 });
